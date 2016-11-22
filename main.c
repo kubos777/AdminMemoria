ffa801312cc1ac206f6d7345965f6ee8be766ef2
Davacas <danielbond09@hotmail.com>
2016-11-22 17:56:09 -0600

    Versión completamente funcional del programa.

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "lista_procs.h"
 #include "memoria.h"
 
@@ -10,8 +11,7 @@ memoria *cambiar_memoria_a_FIFO(memoria *);
 void ejecutar_fifo(memoria *);
 
 //FUNCIÓN MAIN.
-int main (int argc, char *argv[])
-{
+int main (int argc, char *argv[]) {
 	lista *procesos = NULL;
 	memoria *memoria = NULL, *fifo = NULL;
 	int inst_ejec = 0;
@@ -26,13 +26,11 @@ int main (int argc, char *argv[])
 	printf("Situación inicial de la cola de procesos:\n");
 	printQueue(memoria);
 
-	while (++inst_ejec < 11) //Se hará 11 veces porque el quantum es de 5 y eso se cubre dentro de "ejecutar_round_robin"
-	{
+	while (++inst_ejec <= 11) { //Se hará 11 veces porque el quantum es de 5 y eso se cubre dentro de "ejecutar_round_robin"
 		memoria = ejecutar_round_robin(memoria, procesos); //Se pasan todos los procesos a memoria.
 		printf("\t\t\t\t\t      Presione 'enter' para continuar...");
 		getchar(); //Para ver la ejecución del programa.
-		if (getQueueSize(memoria) <= 0)	//Si ya no hay nada en la memoria, se termina el programa.
-		{
+		if (getQueueSize(memoria) <= 0)	{ //Si ya no hay nada en la memoria, se termina el programa.
 			printf("Ya no quedan procesos por despachar.\n");
 			printf("La ejecución del programa termina en este punto.\n");
 			removeList(procesos);
@@ -47,6 +45,8 @@ int main (int argc, char *argv[])
 	fifo = cambiar_memoria_a_FIFO(memoria);
 	printf("Los procesos se ejecutarán de la siguiente forma:\n");
 	printQueue(fifo);
+	printf("\t\t\t\t\t      Presione 'enter' para continuar...");
+	getchar(); //Para ver la ejecución del programa.
 	ejecutar_fifo(fifo);
 
 	printf("Ya no quedan procesos por despachar.\n");
@@ -58,27 +58,23 @@ int main (int argc, char *argv[])
 }
 
 //Función que lee y almacena los "procesos" en una lista.
-lista *leer_archivo()
-{
+lista *leer_archivo(){
 	FILE *archivo = NULL;
 	int pid_temp, size_temp, procs = 0;
 
 	lista *lista = createList(); //Se crea la lista de procesos.
 
 	//Validación y apertura del archivo.
-	if ((archivo = fopen("procesos.txt", "r")) == NULL)
-	{
+	if ((archivo = fopen("procesos.txt", "r")) == NULL){
 		printf("ERROR: No se pudo abrir el archivo que contiene los procesos.\n");
 		printf("La ejecución del programa termina en este punto.\n");
 		exit(0);
 	}
 	//Lectura y almacenamiento de los datos del archivo.
-	while(feof(archivo) == 0)
-	{
+	while(feof(archivo) == 0) {
 		fscanf(archivo, "\t%d %d", &pid_temp, &size_temp);
 		insertAtEnd (pid_temp, size_temp, lista);
-		if (++procs >= 11 || size_temp > 150) //Se valida que no haya más de 10 procesos y que no ocupen más de 10 páginas.
-		{
+		if (++procs >= 11 || size_temp > 150) { //Se valida que no haya más de 10 procesos y que no ocupen más de 10 páginas.
 			printf("ERROR: No hay suficiente memoria para almacenar los procesos.\n");
 			printf("La ejecución del programa termina en este punto.\n");
 			printf("Por favor, elimine algunos procesos del archivo e inténtelo de nuevo.\n");
@@ -92,20 +88,16 @@ lista *leer_archivo()
 }
 
 //Función que pasa los procesos de una lista de procesos a la "memoria RAM"
-memoria *mover_a_memoria(lista *procesos)
-{
+memoria *mover_a_memoria(lista *procesos) {
 	memoria *memoria = createQueue ();	//Se crea la cola que simula la memoria RAM.
 	proceso *aux = procesos->head; 		//Apuntador auxiliar para recorrer la lista de procesos
 	int i, almacenados = 0, ciclos = 0;
 
-	while (memoria->size < 10 && ciclos < 10) 	//Se meterán hasta 10 páginas, una por cada marco en la memoria RAM
-	{
-		for (i = 0; i <= getLength(procesos) - 2; i ++) //Para cada proceso en la lista de procesos
-		{
+	while (memoria->size < 10 && ciclos < 10) { 	//Se meterán hasta 10 páginas, una por cada marco en la memoria RAM
+		for (i = 0; i <= getLength(procesos) - 2; i ++) { //Para cada proceso en la lista de procesos
 			if (aux->paginas <= 0)	//Si ya no tiene páginas, se pasa al siguiente proceso
 				aux = aux->next;
-			else					//De otro modo, se carga una página de ese proceso a la "memoria".
-			{
+			else {					//De otro modo, se carga una página de ese proceso a la "memoria".
 				enqueue(aux->pid, aux->size, aux->paginas, 0, getQueueSize(memoria), 0, memoria); 
 				aux->paginas --;
 				aux = aux->next;
@@ -116,13 +108,11 @@ memoria *mover_a_memoria(lista *procesos)
 		aux = procesos->head;	//Cuando se llega al fin de la lista, se carga la cabeza de la lista al apuntador auxiliar.
 		ciclos ++;
 	}
-
 	return memoria;
 }
 
 //Función que despacha los procesos de la memoria utilizando la técnica "Round-Robin" con un quantum de 5.
-memoria *ejecutar_round_robin(memoria *memoria, lista *procesos)
-{
+memoria *ejecutar_round_robin(memoria *memoria, lista *procesos){
 	int i;
 	marco *ejecutar = dequeue(memoria);
 	marco *aux = memoria->front;
@@ -131,10 +121,9 @@ memoria *ejecutar_round_robin(memoria *memoria, lista *procesos)
 	ejecutar->size -= 5;		//Se ejecutan 5 instrucciones, reduce el tamaño del proceso.
 	ejecutar->inst_ejec += 5;	//Se actualiza el "apuntador de instrucciones".
 	ejecutar->desplazamiento += 5;	//Se actualiza el desplazamiento sobre el bloque
-	if (ejecutar->desplazamiento % 15 == 0) 	
-	{//Si ya se todas las instrucciones en un bloque
-		if (ejecutar->paginas >= 15) //Si aún hay páginas por cargar
-		{
+
+	if (ejecutar->desplazamiento % 15 == 0) { //Si ya se todas las instrucciones en un bloque
+		if (ejecutar->paginas >= 1) { //Si aún hay páginas por cargar
 			ejecutar->paginas --; //se carga una nueva pagina a memoria.
 			enqueue(ejecutar->pid, ejecutar->size, ejecutar->paginas, ejecutar->inst_ejec, ejecutar->bloque, ejecutar->desplazamiento, memoria);
 			printf("Terminó la ejecución del proceso con PID %d. Se quedó en:\n", ejecutar->pid);
@@ -142,8 +131,7 @@ memoria *ejecutar_round_robin(memoria *memoria, lista *procesos)
 			printf("Dirección real: %d\n", 1000 + (ejecutar->bloque*15) + ejecutar->desplazamiento);
 			printf("Se cargó una nueva página para el proceso con PID %d\n", ejecutar->pid);
 		}
-		else	//Si ya no hay páginas por cargar, se carga una nueva de otro proceso.
-		{
+		else {	//Si ya no hay páginas por cargar, se carga una nueva de otro proceso.
 			ejecutar->paginas --;
 			printf("Terminó la ejecución del proceso con PID %d. Se quedó en:\n", ejecutar->pid);
 			printf("Dirección virtual: (%d, %d). ", ejecutar->bloque, ejecutar->desplazamiento);
@@ -152,19 +140,16 @@ memoria *ejecutar_round_robin(memoria *memoria, lista *procesos)
 			mover_a_memoria(procesos);
 		}
 	}
-	else if (ejecutar->size <= 0) //Si ya no quedan instrucciones para ejecutarse
-	{
+	else if (ejecutar->size <= 0){ //Si ya no quedan instrucciones para ejecutarse
 		printf("AVISO: El proceso con PID %d pasó a estado de 'terminado'.\n", ejecutar->pid);
 		printf("Terminó su ejecución estando en las direcciones:\n");
 		printf("Dirección virtual: (%d, %d). ", ejecutar->bloque, ejecutar->desplazamiento + ejecutar->size);
 		printf("Dirección real: %d\n", 1000 + (ejecutar->bloque*15) + (ejecutar->desplazamiento + ejecutar->size));
 
 		if(getQueueSize(memoria) > 0)	//Si aún hay procesos por despachar,
-			for (i = 0; i <= getQueueSize(memoria)+1; i ++) //se checa cada proceso en la cola de procesos,
-			{
+			for (i = 0; i <= getQueueSize(memoria)+1; i ++){ //se checa cada proceso en la cola de procesos,
 				marco *eliminar = dequeue(memoria);
-				if (eliminar->pid == ejecutar->pid)	//si un proceso en la cola tiene el mismo PID que el que ya terminó
-				{
+				if (eliminar->pid == ejecutar->pid) {	//si un proceso en la cola tiene el mismo PID que el que ya terminó
 					removeMarco(eliminar);
 					eliminar = eliminar->next;		//éste tambien se elimina de la cola de procesos,
 				}
@@ -172,48 +157,42 @@ memoria *ejecutar_round_robin(memoria *memoria, lista *procesos)
 					enqueue(eliminar->pid, eliminar->size, eliminar->paginas, eliminar->inst_ejec, eliminar->bloque, eliminar->desplazamiento, memoria);				
 			}
 	}
-	else	//Si no hay que cargar páginas y aún hay instrucciones por ejecutarse, 
-	{		//el proceso sólo entra a la cola de nuevo.
+	else {	//Si no hay que cargar páginas y aún hay instrucciones por ejecutarse, 
+			//el proceso sólo entra a la cola de nuevo.
 		enqueue(ejecutar->pid, ejecutar->size, ejecutar->paginas, ejecutar->inst_ejec, ejecutar->bloque, ejecutar->desplazamiento, memoria);
 		printf("Terminó la ejecución del proceso con PID %d. Se quedó en:\n", ejecutar->pid);
 		printf("Dirección virtual: (%d, %d). ", ejecutar->bloque, ejecutar->desplazamiento);
 		printf("Dirección real: %d\n", 1000 + (ejecutar->bloque*15) + ejecutar->desplazamiento);
-
 	}
 
 	if (getQueueSize(memoria) > 1)	//Si hay más de un proceso por despachar,
-		for (i = 0; i <= getQueueSize(memoria)-1; i ++) //se busca en cada elemento de la cola de procesos
-		{
-			if (ejecutar->pid == aux->pid)	//si un proceso tiene el mismo PID que el que se estaba ejecutando
-			{
+		for (i = 0; i <= getQueueSize(memoria)-1; i ++) {//se busca en cada elemento de la cola de procesos
+			if (ejecutar->pid == aux->pid) {	//si un proceso tiene el mismo PID que el que se estaba ejecutando
 				aux->size = ejecutar->size;	// y se actualizan sus datos.
 				aux->paginas = ejecutar->paginas;
 				aux->inst_ejec = ejecutar->inst_ejec;
 			}
 			aux = aux->next;
 		}
-
+	printf("%d\n", ejecutar->size);
 	removeMarco(ejecutar);
 	return memoria;
 }
 
-memoria *cambiar_memoria_a_FIFO(memoria *rr)
-{
+memoria *cambiar_memoria_a_FIFO(memoria *rr) {
 	int pag_cargadas = 0, i, repetido;
 	memoria *fifo = createQueue();	//Se crea una nueva cola.
 	marco *recorre = NULL;			//Apuntador que recorrerá la nueva cola (FIFO).
 	marco *cargar = rr->front;		//Apuntador que recorrerá la cola anterior (RR).
 
-	enqueue(cargar->pid, cargar->size, cargar->paginas, cargar->inst_ejec, cargar->bloque, cargar->desplazamiento, fifo);
+	enqueue(cargar->pid, cargar->size, cargar->size/15, cargar->inst_ejec, cargar->bloque, cargar->desplazamiento, fifo);
 	
 	recorre = fifo->front;
 	cargar = cargar->next;
-	while (cargar != NULL) //Mientras el proceso a cargar no sea nulo...
-	{
-		recorre = fifo->front; //El apuntador de la nueva cola será igual a su elemento de adelante
+	while (cargar != NULL) { 					//Mientras el proceso a cargar no sea nulo...
+		recorre = fifo->front; 					//El apuntador de la nueva cola será igual a su elemento de adelante
 		for (i = 0; i < getQueueSize(fifo); i ++) //Se checa cada proceso en la nueva cola.
-			if (cargar->pid == recorre->pid)	//Si un proceso en la cola tiene el mismo PID que el que se desea cargar
-			{
+			if (cargar->pid == recorre->pid) {	//Si un proceso en la cola tiene el mismo PID que el que se desea cargar.
 				recorre = recorre->next;		
 				repetido = 1;					//se marca como repetido
 			}
@@ -221,33 +200,32 @@ memoria *cambiar_memoria_a_FIFO(memoria *rr)
 				recorre = recorre->next;
 
 		if (repetido == 0) //Si el proceso a cargar no estaba en la nueva cola, se carga.
-			enqueue(cargar->pid, cargar->size, cargar->paginas, cargar->inst_ejec, cargar->bloque, cargar->desplazamiento, fifo);
+			enqueue(cargar->pid, cargar->size, cargar->size/15, cargar->inst_ejec, cargar->bloque, cargar->desplazamiento, fifo);
 		
 		repetido = 0;
 		cargar = cargar->next;
 	}
 
 	recorre = fifo->front;
-	while (recorre->next != NULL)
-	{
-		printf("Se reccorió %d páginas al proceso con PID %d para encolar al proceso con PID %d\n", recorre->paginas, recorre->next->pid, recorre->pid);
+	while (recorre->next != NULL) {
+		printf("El proceso con PID %d aún tiene %d instrucciones por ejecutar.\n", recorre->pid, recorre->size);
+		printf("Se recorrió %d páginas al proceso con PID %d para encolarlo.\n", recorre->paginas, recorre->next->pid);
 		recorre = recorre->next;
 	}
 	return fifo;
 }
 
-void ejecutar_fifo(memoria *fifo)
-{
+void ejecutar_fifo(memoria *fifo) {
 	marco *ejecutar = dequeue(fifo);
-	while (ejecutar != NULL)
-	{
+	while (ejecutar != NULL) {
 		printf("Terminó la ejecución del proceso con PID %d. Se quedó en:\n", ejecutar->pid);
-		printf("Dirección virtual: (%d, %d). ", ejecutar->paginas-1, ejecutar->size%15);
-		printf("Dirección real: %d\n", 1000 + (ejecutar->paginas-1)*15 + ejecutar->size%15);
-		printf("Se recorrerán los procesos hasta el inicio de la cola.\n");
+		printf("Dirección virtual: (%d, %d). ", ejecutar->paginas, ejecutar->size%15);
+		printf("Dirección real: %d\n", 1000 + ejecutar->size);
+		printf("Se recorrerán los procesos restantes hasta el inicio de la cola.\n");
 		printf("\t\t\t\t\t      Presione 'enter' para continuar...");
+		ejecutar->size = 0;
+		ejecutar->paginas = 0;
 		getchar(); //Para ver la ejecución del programa.
 		ejecutar = dequeue(fifo);
 	}
-
 }
